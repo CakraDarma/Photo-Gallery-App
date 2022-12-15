ea50f43d902f63172f1382aa05632b25d878a8fa
CakraDarma <baguscakraa02@gmail.com>
2022-12-16 04:51:26 +0800

    fix2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import EditPhoto from './routes/EditPhoto';
 import Home from './routes/Home';
 import Photos from './routes/Photos';
@@ -7,30 +7,30 @@ import AddPhoto from './routes/AddPhoto';
 import NotFound from './routes/NotFound';
 
 const App = () => {
-  return (
-    <>
-      <div className='navbar'>
-        <div className='studentInfo'>
-          <h2 className='studentName'>Ananda Putra Brahmana</h2>
-          <p className='studentId'>FE2533792</p>
-          {/* TODO: answer here */}
-        </div>
-        <Link to='/'>Home</Link>
-        <Link to='/photos'>My Photos</Link>
-        <Link to='/add'>Add Photo</Link>
-      </div>
+	return (
+		<>
+			<div className='navbar'>
+				<div className='studentInfo'>
+					<h2 className='studentName'>cakradarma</h2>
+					<p className='studentId'>FE2595211</p>
+					{/* TODO: answer here */}
+				</div>
+				<Link to='/'>Home</Link>
+				<Link to='/photos'>My Photos</Link>
+				<Link to='/add'>Add Photo</Link>
+			</div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/photos">
-          <Route index element={<Photos />} />
-          <Route path=":id" element={<EditPhoto />} />
-        </Route>
-        <Route path="/add" element={<AddPhoto />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/photos'>
+					<Route index element={<Photos />} />
+					<Route path=':id' element={<EditPhoto />} />
+				</Route>
+				<Route path='/add' element={<AddPhoto />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</>
+	);
 };
 
 export default App;
