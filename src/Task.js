01fb1e6c9bfc776a3409abf26a80a90e3a99d14c
CakraDarma <baguscakraa02@gmail.com>
2022-12-16 04:44:53 +0800

    fix

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://consumingrestfulapi.netlify.app" // TODO: replace this
+const studentName = "Ananda Putra Brahmana" // TODO: replace this
+const studentId = "FE2533792" // TODO: replace this
+
+export { url, studentName, studentId }
